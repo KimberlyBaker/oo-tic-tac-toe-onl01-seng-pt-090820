87335591e878c3c74772f931d282c24458ab4856
Kimberly Baker <bakerk2010@gmail.com>
2020-11-09 22:06:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,7 +93,7 @@ class TicTacToe
   end
   
   def full?
-    @board.all {|index| index == "X" || index == "O"}
+    @board.all? {|index| index == "X" || index == "O"}
   end
   
   def draw?
@@ -109,6 +109,20 @@ class TicTacToe
       return true
     else
       return false
+    end
+  end
+  
+  def winner
+    index = []
+    index = won?
+    if index == false
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
     end
   end
   
