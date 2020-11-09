c9d4005fdda4fd55abd0ab0367eaad67cdadbe94
Kimberly Baker <bakerk2010@gmail.com>
2020-11-09 19:55:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,11 +65,11 @@ class TicTacToe
     user_input = gets.chomp
     index = input_to_index(user_input)
     if valid_move?(index)
-    player_token = current_player
-    move(index, player_token)
-    display_board
-  else
-    turn
+      player_token = current_player
+      move(index, player_token)
+      display_board
+    else
+      turn
   end
   end
   
